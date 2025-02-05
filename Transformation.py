d252244392a691feb0d850f03d4b1cf6b969f8c9
Marc Semadeni <marc.semadeni@students.snow.edu>
2025-02-05 09:22:35 -0700

    Import sesh

--- Transformation.py
+++ Transformation.py
@@ -1,5 +1,21 @@
 import math
 import pandas as pd
-import matplotlib as plt
+import matplotlib
+import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
+import numpy as np
 
+data = pd.read_csv('Mewing data set.csv')
+data.fillna(0, inplace=True)
+print(data.head())
+
+x = data['Comp Time']
+y = data.salary
+# plt.scatter(x,y)
+# plt.show()
+
+x = sm.add_constant(x)
+model = sm.OLS(y, x).fit()
+
+print(model.summary())
