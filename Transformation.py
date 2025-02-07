451fa61970f457412b9138d0fed444c188969944
Marc Semadeni <marc.semadeni@students.snow.edu>
2025-02-06 17:12:06 -0700

    Added line to scatterplot

--- Transformation.py
+++ Transformation.py
@@ -8,14 +8,22 @@ import numpy as np
 
 data = pd.read_csv('Mewing data set.csv')
 data.fillna(0, inplace=True)
-print(data.head())
+# print(data.head())
 
 x = data['Comp Time']
 y = data.salary
-# plt.scatter(x,y)
-# plt.show()
 
-x = sm.add_constant(x)
-model = sm.OLS(y, x).fit()
+plt.scatter(x,y)
+plt.xlabel("Comp Time")
+plt.ylabel("Salary")
+plt.title("Scatter plot of Comp Time and Salary")
+m, b = np.polyfit(x, y, 1)
+plt.plot(x, m*x + b, color="red")
+plt.show()
 
-print(model.summary())
+#Adjusted r-squared .489
+
+# x = sm.add_constant(x)
+# model = sm.OLS(y, x).fit()
+
+# print(model.summary())
