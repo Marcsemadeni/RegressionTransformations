963f7658b9d8df9c58e5bd8959b07c510349c91f
Marc Semadeni <marc.semadeni@students.snow.edu>
2025-02-06 17:53:11 -0700

    Finished question 2

--- Transformation.py
+++ Transformation.py
@@ -10,18 +10,62 @@ data = pd.read_csv('Mewing data set.csv')
 data.fillna(0, inplace=True)
 # print(data.head())
 
-x = data['Comp Time']
+X = data['Comp Time']
 y = data.salary
-
-plt.scatter(x,y)
-plt.xlabel("Comp Time")
-plt.ylabel("Salary")
-plt.title("Scatter plot of Comp Time and Salary")
-sns.regplot(x=x,y=y, line_kws={"color":"red"})
-plt.show()
+# x=X
+# plt.scatter(x,y)
+# plt.xlabel("Comp Time")
+# plt.ylabel("Salary")
+# plt.title("Scatter plot of Comp Time and Salary")
+# sns.regplot(x=x,y=y, line_kws={"color":"red"})
+# plt.show()
 
 #Adjusted r-squared .489
 
-x = sm.add_constant(x)
-model = sm.OLS(y, x).fit()
+# x = sm.add_constant(x)
+# model = sm.OLS(y, x).fit()
+# print(model.summary())
+
+# log_x=np.log(X)
+# plt.scatter(log_x,y)
+# plt.xlabel("Comp Time")
+# plt.ylabel("Salary")
+# plt.title("Scatter plot of Comp Time and Salary")
+# sns.regplot(x=log_x,y=y, line_kws={"color":"red"})
+# plt.show()
+
+# #Adjusted r-squared: DOESNT WORK
+
+# log_x = sm.add_constant(log_x)
+# model = sm.OLS(y, log_x).fit()
+# print(model.summary())
+
+
+# x_squared=X**2
+# plt.scatter(x_squared,y)
+# plt.xlabel("Comp Time")
+# plt.ylabel("Salary")
+# plt.title("Scatter plot of Comp Time and Salary")
+# sns.regplot(x=x_squared,y=y, line_kws={"color":"red"})
+# plt.show()
+
+# #Adjusted r-squared: 0.644
+
+# x_squared = sm.add_constant(x_squared)
+# model = sm.OLS(y, x_squared).fit()
+# print(model.summary())
+
+
+power_of_x=1.05**X
+plt.scatter(power_of_x,y)
+plt.xlabel("Comp Time")
+plt.ylabel("Salary")
+plt.title("Scatter plot of Comp Time and Salary")
+sns.regplot(x=power_of_x,y=y, line_kws={"color":"red"})
+plt.show()
+
+#Adjusted r-squared: 0.669
+
+power_of_x = sm.add_constant(power_of_x)
+model = sm.OLS(y, power_of_x).fit()
 print(model.summary())
