0355486ebdeb8ef4898445b3353bf0061178cda4
= <=>
2025-02-07 16:46:52 -0700

    Added more comments

--- Transformation.py
+++ Transformation.py
@@ -30,12 +30,17 @@ y = data.salary
 # sns.regplot(x=x,y=y, line_kws={"color":"red"})
 # plt.show()
 
-#Adjusted r-squared .489
+# Adjusted r-squared .489
+
+# ------------------------------
+# Step 2: Linear Transformations
+# ------------------------------
 
 # x = sm.add_constant(x)
 # model = sm.OLS(y, x).fit()
 # print(model.summary())
 
+# ---- Logarithmic Transformation -----
 log_x=np.log1p(X)
 plt.scatter(log_x,y)
 plt.xlabel("Comp Time")
@@ -44,13 +49,14 @@ plt.title("Scatter plot of Comp Time and Salary")
 sns.regplot(x=log_x,y=y, line_kws={"color":"red"})
 plt.show()
 
-#Adjusted r-squared: .199
+# Adjusted r-squared: .199
 
 log_x = sm.add_constant(log_x)
 model = sm.OLS(y, log_x).fit()
 print(model.summary())
 
 
+# ---- Exponential Transformation -----
 # x_squared=X**2
 # plt.scatter(x_squared,y)
 # plt.xlabel("Comp Time")
@@ -59,13 +65,13 @@ print(model.summary())
 # sns.regplot(x=x_squared,y=y, line_kws={"color":"red"})
 # plt.show()
 
-# #Adjusted r-squared: 0.644
+# Adjusted r-squared: 0.644
 
 # x_squared = sm.add_constant(x_squared)
 # model = sm.OLS(y, x_squared).fit()
 # print(model.summary())
 
-
+# ---- Power Transformation -----
 # power_of_x=1.05**X
 # plt.scatter(power_of_x,y)
 # plt.xlabel("Comp Time")
@@ -74,7 +80,7 @@ print(model.summary())
 # sns.regplot(x=power_of_x,y=y, line_kws={"color":"red"})
 # plt.show()
 
-# #Adjusted r-squared: 0.669
+# Adjusted r-squared: 0.669
 
 # power_of_x = sm.add_constant(power_of_x)
 # model = sm.OLS(y, power_of_x).fit()
